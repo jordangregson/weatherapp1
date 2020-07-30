fba45430ef88aebc990e625ff57c55cc7b56de7e
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2020-07-29 22:09:01 -0400

    The weather data now works

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-async function weather_data() {
-    const api_key = "554d1f11a8df60eddbddf39182838a79";
-    const api_url = "api.openweathermap.org/data/2.5/weather?q={Londom}&appid={"+api_key+"}";
-    const response = await fetch(api_url);
-    const data = await response.json();
-    console.log(data);
+let lat;
+let lng;
+
+let weatherDesc;
+let temp;
+
+
+function geocodeLatLng() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                let lat = position.coords.latitude;
+                let lng = position.coords.longitude;
+
+                const api_url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=imperial&appid=554d1f11a8df60eddbddf39182838a79`;
+
+                fetch(api_url)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        //console.log(data);
+
+                        
+                        temp = document.getElementById("temp");
+                        temp.innerHTML = Math.round(data.main.temp);
+
+                        weatherDesc = document.getElementById("condition");
+                        weatherDesc.innerHTML = data.weather[0].description;
+
+                        console.log(data.main);
+                    })
+
+            })
+    }
+    else {
+        console("Geolocation doesn't work")
+    }
+
+
 }
 
-weather_data();
+geocodeLatLng();
+
